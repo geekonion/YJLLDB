5b67b3ed2dbb0a8db74365156188f150b9a1bef1
xyj <yyth-003@163.com>
2023-09-11 11:10:19 +0800

    支持搜索整个Macho；支持指定module

--- src/LookupBytes.py
+++ src/LookupBytes.py
@@ -42,6 +42,8 @@ def lookup_bytes(debugger, command, result, internal_dict):
     bytes_len = len(bytes_list)
     input_bytes = bytes(bytes_list)
 
+    lookup_module = options.module
+
     print('lookup bytes, this may take a while')
     target = debugger.GetSelectedTarget()
     bundle_path = target.GetExecutable().GetDirectory()
@@ -49,44 +51,49 @@ def lookup_bytes(debugger, command, result, internal_dict):
     for module in target.module_iter():
         module_file_spec = module.GetFileSpec()
 
-        module_dir = module_file_spec.GetDirectory()
-        if bundle_path not in module_dir:
-            continue
-
         name = module_file_spec.GetFilename()
-        if name.startswith('libswift'):
-            continue
+        if lookup_module:
+            if lookup_module not in name:
+                continue
+        else:
+            module_dir = module_file_spec.GetDirectory()
+            if bundle_path not in module_dir:
+                continue
+
+            if name.startswith('libswift'):
+                continue
 
         hits_count = 0
         result.AppendMessage("-----try to lookup bytes in %s-----" % name)
         for seg in module.section_iter():
             seg_name = seg.GetName()
-            if seg_name != "__TEXT":
-                continue
-
-            for sec in seg:
-                sec_name = sec.GetName()
-                if "_stub" in sec_name or \
-                        "__objc_methname" == sec_name or \
-                        "__objc_classname" == sec_name or \
-                        "__objc_methtype" == sec_name or \
-                        "__cstring" == sec_name or \
-                        "__ustring" == sec_name or \
-                        "__gcc_except_tab" == sec_name or \
-                        "__const" == sec_name or \
-                        "__unwind_info" == sec_name:
+            search_all = options.all
+            if search_all:
+                if seg_name == '__PAGEZERO':
+                    continue
+            else:
+                if seg_name != "__TEXT":
                     continue
 
-                sec_addr = sec.GetLoadAddress(target)
+            def match_seg(in_seg, in_seg_name, in_sec_name):
+                matched_count = 0
+                sec_addr = in_seg.GetLoadAddress(target)
                 error = lldb.SBError()
-                sec_size = sec.GetByteSize()
+                sec_size = in_seg.GetByteSize()
+                if sec_size == 0:
+                    if len(in_sec_name):
+                        result.AppendMessage('ignore empty sec {}.{}'.format(in_seg_name, in_sec_name))
+                    else:
+                        result.AppendMessage('ignore empty seg {}'.format(in_seg_name))
+                    return 0
 
                 # 砸壳应用读取不到
                 # sec_data = sec.GetSectionData().ReadRawData(error, 0, sec_size)
                 sec_data = target.ReadMemory(lldb.SBAddress(sec_addr, target), sec_size, error)
                 if not error.Success():
-                    result.AppendMessage('read section {}:0x{:x} failed! {}'.format(sec_name, sec_addr, error.GetCString()))
-                    continue
+                    result.AppendMessage(
+                        'read section {}:0x{:x} failed! {}'.format(in_sec_name, sec_addr, error.GetCString()))
+                    return 0
 
                 pos = 0
                 while True:
@@ -94,16 +101,44 @@ def lookup_bytes(debugger, command, result, internal_dict):
                     if pos == -1:
                         break
 
-                    hits_count += 1
-                    total_count += 1
+                    matched_count += 1
                     bytes_addr = pos + sec_addr
                     inst_addr = target.ResolveLoadAddress(bytes_addr)
-                    result.AppendMessage('address = 0x{:x} where = {}'.format(bytes_addr, inst_addr))
+                    addr_info = '{}'.format(inst_addr)
+                    if addr_info:
+                        result.AppendMessage('address = 0x{:x} where = {}'.format(bytes_addr, inst_addr))
+                    else:
+                        result.AppendMessage('address = 0x{:x} where = {}'.format(bytes_addr, seg_name))
 
                     pos += bytes_len
 
+                return matched_count
+
+            if search_all:
+                matched_count = match_seg(seg, seg_name, '')
+                total_count += matched_count
+                hits_count += matched_count
+            else:
+                for sec in seg:
+                    sec_name = sec.GetName()
+
+                    if "_stub" in sec_name or \
+                            "__objc_methname" == sec_name or \
+                            "__objc_classname" == sec_name or \
+                            "__objc_methtype" == sec_name or \
+                            "__cstring" == sec_name or \
+                            "__ustring" == sec_name or \
+                            "__gcc_except_tab" == sec_name or \
+                            "__const" == sec_name or \
+                            "__unwind_info" == sec_name:
+                        continue
+
+                    matched_count = match_seg(sec, seg_name, sec_name)
+                    total_count += matched_count
+                    hits_count += matched_count
+
         if hits_count == 0:
-            result.AppendMessage("input bytes not found")
+            result.AppendMessage("input bytes not found in {}".format(name))
 
     result.AppendMessage("{} locations found".format(total_count))
 
@@ -116,5 +151,14 @@ def generate_option_parser():
             "\t%prog c0 03 5f d6"
 
     parser = optparse.OptionParser(usage=usage, prog='blookup')
+    parser.add_option("-a", "--all",
+                      action="store_true",
+                      default=False,
+                      dest="all",
+                      help="lookup bytes in all segments, default in __TEXT")
+    parser.add_option("-m", "--module",
+                      action="store",
+                      dest="module",
+                      help="lookup bytes in the specified module")
 
     return parser
