def38b039b245c301955fced07d8ec96c2266c4d
xyj <yyth-003@163.com>
2024-01-11 16:33:07 +0800

    提取工具函数

--- src/FunctionStarts.py
+++ src/FunctionStarts.py
@@ -4,7 +4,7 @@ import lldb
 import optparse
 import shlex
 import util
-import MachO
+import MachOHelper
 
 
 def __lldb_init_module(debugger, internal_dict):
@@ -39,97 +39,15 @@ def dump_function_starts(debugger, command, result, internal_dict):
         file_spec = target.GetExecutable()
         lookup_module_name = file_spec.GetFilename()
 
-    byte_order = 'little' if target.GetByteOrder() == lldb.eByteOrderLittle else 'big'
-    total_count = 0
-    for module in target.module_iter():
-        module_file_spec = module.GetFileSpec()
-        module_name = module_file_spec.GetFilename()
-
-        if lookup_module_name not in module_name:
-            continue
-        seg = module.FindSection('__TEXT')
-        if not seg:
-            result.AppendMessage('seg __TEXT not found in {}'.format(module_name))
-            continue
-
-        result.AppendMessage("-----parsing module %s-----" % module_name)
-        header_addr = seg.GetLoadAddress(target)
-        slide = header_addr - seg.GetFileAddress()
-
-        first_sec = seg.GetSubSectionAtIndex(0)
-        sec_addr = first_sec.GetLoadAddress(target)
-
-        error = lldb.SBError()
-        header_size = sec_addr - header_addr
-        header_data = target.ReadMemory(lldb.SBAddress(header_addr, target), header_size, error)
-        if not error.Success():
-            result.AppendMessage('read header failed! {}'.format(error.GetCString()))
-            break
-
-        info = MachO.parse_header(header_data)
-
-        lcs = info['lcs']
-        for lc in lcs:
-            cmd = lc['cmd']
-            if cmd != '19':  # LC_SEGMENT_64
-                continue
-
-            seg_name = lc['name']
-            if seg_name != '__LINKEDIT':
-                continue
-
-            linkedit_offset = int(lc['offset'], 16)
-            linkedit_vmaddr = int(lc['vmaddr'], 16)
-
-            sects = lc['sects']
-            for sect in sects:
-                sec_name = sect['name']
-                if sec_name != 'Function Starts':
-                    continue
-
-                dataoff = int(sect['offset'], 16)
-                datasize = int(sect['size'], 16)
-                data_start = linkedit_vmaddr + slide + dataoff - linkedit_offset
-
-                error1 = lldb.SBError()
-                data_bytes = target.ReadMemory(lldb.SBAddress(data_start, target), datasize, error1)
-                if not error1.Success():
-                    result.AppendMessage('read data failed! {}'.format(error1.GetCString()))
-                    break
-
-                file_offset = 0
-                idx = 0
-                while idx < datasize:
-                    offset = 0
-                    bit = 0
-                    while idx < datasize:
-                        byte = data_bytes[idx: idx + 1]
-                        byte_value = int.from_bytes(byte, byteorder=byte_order)
-                        if byte_value == 0:
-                            idx += 1
-                            continue
-
-                        slice = byte_value & 0x7f
-
-                        if bit >= 64 or slice << bit >> bit != slice:
-                            pass
-                        else:
-                            offset |= (slice << bit)
-                            bit += 7
-
-                        idx += 1
-                        if byte_value & 0x80 == 0:
-                            break
-
-                    if offset > 0:
-                        file_offset += offset
-                        func_start = header_addr + file_offset
-                        func_addr = target.ResolveLoadAddress(func_start)
-                        result.AppendMessage('address = 0x{:x} where = {}'.format(func_start, func_addr))
-                        total_count += 1
-                break
-
-            break
+    funcs, module_file_spec = MachOHelper.get_function_starts(result, target, lookup_module_name)
+    if not funcs:
+        result.AppendMessage("module {} not found".format(lookup_module_name))
+    else:
+        total_count = 0
+        for func_start in funcs:
+            func_addr = target.ResolveLoadAddress(func_start)
+            result.AppendMessage('address = 0x{:x} where = {}'.format(func_start, func_addr))
+            total_count += 1
 
         result.AppendMessage('{} function(s) found'.format(total_count))
 
---
+++ src/MachOHelper.py
@@ -0,0 +1,104 @@
+# -*- coding: UTF-8 -*-
+
+import lldb
+import MachO
+
+
+def get_function_starts(result, target, lookup_module_name):
+    byte_order = 'little' if target.GetByteOrder() == lldb.eByteOrderLittle else 'big'
+    funcs = None
+    module_file_spec = None
+    for module in target.module_iter():
+        module_file_spec = module.GetFileSpec()
+        module_name = module_file_spec.GetFilename()
+
+        if lookup_module_name not in module_name:
+            continue
+
+        funcs = []
+        seg = module.FindSection('__TEXT')
+        if not seg:
+            result.AppendWarning('seg __TEXT not found in {}'.format(module_name))
+            continue
+
+        result.AppendMessage("-----parsing module %s-----" % module_name)
+        header_addr = seg.GetLoadAddress(target)
+        slide = header_addr - seg.GetFileAddress()
+
+        first_sec = seg.GetSubSectionAtIndex(0)
+        sec_addr = first_sec.GetLoadAddress(target)
+
+        error = lldb.SBError()
+        header_size = sec_addr - header_addr
+        header_data = target.ReadMemory(lldb.SBAddress(header_addr, target), header_size, error)
+        if not error.Success():
+            result.AppendWarning('read header failed! {}'.format(error.GetCString()))
+            break
+
+        info = MachO.parse_header(header_data)
+
+        lcs = info['lcs']
+        for lc in lcs:
+            cmd = lc['cmd']
+            if cmd != '19':  # LC_SEGMENT_64
+                continue
+
+            seg_name = lc['name']
+            if seg_name != '__LINKEDIT':
+                continue
+
+            linkedit_offset = int(lc['offset'], 16)
+            linkedit_vmaddr = int(lc['vmaddr'], 16)
+
+            sects = lc['sects']
+            for sect in sects:
+                sec_name = sect['name']
+                if sec_name != 'Function Starts':
+                    continue
+
+                dataoff = int(sect['offset'], 16)
+                datasize = int(sect['size'], 16)
+                data_start = linkedit_vmaddr + slide + dataoff - linkedit_offset
+
+                error1 = lldb.SBError()
+                data_bytes = target.ReadMemory(lldb.SBAddress(data_start, target), datasize, error1)
+                if not error1.Success():
+                    result.AppendWarning('read data failed! {}'.format(error1.GetCString()))
+                    break
+
+                file_offset = 0
+                idx = 0
+                while idx < datasize:
+                    offset = 0
+                    bit = 0
+                    while idx < datasize:
+                        byte = data_bytes[idx: idx + 1]
+                        byte_value = int.from_bytes(byte, byteorder=byte_order)
+                        if byte_value == 0:
+                            idx += 1
+                            continue
+
+                        slice = byte_value & 0x7f
+
+                        if bit >= 64 or slice << bit >> bit != slice:
+                            pass
+                        else:
+                            offset |= (slice << bit)
+                            bit += 7
+
+                        idx += 1
+                        if byte_value & 0x80 == 0:
+                            break
+
+                    if offset > 0:
+                        file_offset += offset
+                        func_start = header_addr + file_offset
+                        funcs.append(func_start)
+                # sects
+                break
+            # lcs
+            break
+        # modules
+        break
+
+    return funcs, module_file_spec
