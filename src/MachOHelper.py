fe58c1e9e275809646eba04d1a30018ff87717bc
xyj <yyth-003@163.com>
2024-01-11 17:24:09 +0800

    拆分函数

--- src/MachOHelper.py
+++ src/MachOHelper.py
@@ -5,9 +5,72 @@ import MachO
 
 
 def get_function_starts(result, target, lookup_module_name):
-    byte_order = 'little' if target.GetByteOrder() == lldb.eByteOrderLittle else 'big'
     funcs = None
+    module_file_spec, header_addr, slide, segment_info = get_segment_info(result, target, lookup_module_name, '__LINKEDIT')
+    if not module_file_spec:
+        return funcs, module_file_spec
+
+    funcs = []
+    byte_order = 'little' if target.GetByteOrder() == lldb.eByteOrderLittle else 'big'
+    linkedit_offset = int(segment_info['offset'], 16)
+    linkedit_vmaddr = int(segment_info['vmaddr'], 16)
+
+    sects = segment_info['sects']
+    for sect in sects:
+        sec_name = sect['name']
+        if sec_name != 'Function Starts':
+            continue
+
+        dataoff = int(sect['offset'], 16)
+        datasize = int(sect['size'], 16)
+        data_start = linkedit_vmaddr + slide + dataoff - linkedit_offset
+
+        error1 = lldb.SBError()
+        data_bytes = target.ReadMemory(lldb.SBAddress(data_start, target), datasize, error1)
+        if not error1.Success():
+            result.AppendWarning('read data failed! {}'.format(error1.GetCString()))
+            break
+
+        file_offset = 0
+        idx = 0
+        while idx < datasize:
+            offset = 0
+            bit = 0
+            while idx < datasize:
+                byte = data_bytes[idx: idx + 1]
+                byte_value = int.from_bytes(byte, byteorder=byte_order)
+                if byte_value == 0:
+                    idx += 1
+                    continue
+
+                slice = byte_value & 0x7f
+
+                if bit >= 64 or slice << bit >> bit != slice:
+                    pass
+                else:
+                    offset |= (slice << bit)
+                    bit += 7
+
+                idx += 1
+                if byte_value & 0x80 == 0:
+                    break
+
+            if offset > 0:
+                file_offset += offset
+                func_start = header_addr + file_offset
+                funcs.append(func_start)
+        # sects
+        break
+
+    return funcs, module_file_spec
+
+
+def get_segment_info(result, target, lookup_module_name, target_seg_name):
     module_file_spec = None
+    header_addr = 0
+    slide = 0
+    segment_info = None
+
     for module in target.module_iter():
         module_file_spec = module.GetFileSpec()
         module_name = module_file_spec.GetFilename()
@@ -15,7 +78,6 @@ def get_function_starts(result, target, lookup_module_name):
         if lookup_module_name not in module_name:
             continue
 
-        funcs = []
         seg = module.FindSection('__TEXT')
         if not seg:
             result.AppendWarning('seg __TEXT not found in {}'.format(module_name))
@@ -44,61 +106,13 @@ def get_function_starts(result, target, lookup_module_name):
                 continue
 
             seg_name = lc['name']
-            if seg_name != '__LINKEDIT':
+            if seg_name != target_seg_name:
                 continue
 
-            linkedit_offset = int(lc['offset'], 16)
-            linkedit_vmaddr = int(lc['vmaddr'], 16)
-
-            sects = lc['sects']
-            for sect in sects:
-                sec_name = sect['name']
-                if sec_name != 'Function Starts':
-                    continue
-
-                dataoff = int(sect['offset'], 16)
-                datasize = int(sect['size'], 16)
-                data_start = linkedit_vmaddr + slide + dataoff - linkedit_offset
-
-                error1 = lldb.SBError()
-                data_bytes = target.ReadMemory(lldb.SBAddress(data_start, target), datasize, error1)
-                if not error1.Success():
-                    result.AppendWarning('read data failed! {}'.format(error1.GetCString()))
-                    break
-
-                file_offset = 0
-                idx = 0
-                while idx < datasize:
-                    offset = 0
-                    bit = 0
-                    while idx < datasize:
-                        byte = data_bytes[idx: idx + 1]
-                        byte_value = int.from_bytes(byte, byteorder=byte_order)
-                        if byte_value == 0:
-                            idx += 1
-                            continue
-
-                        slice = byte_value & 0x7f
-
-                        if bit >= 64 or slice << bit >> bit != slice:
-                            pass
-                        else:
-                            offset |= (slice << bit)
-                            bit += 7
-
-                        idx += 1
-                        if byte_value & 0x80 == 0:
-                            break
-
-                    if offset > 0:
-                        file_offset += offset
-                        func_start = header_addr + file_offset
-                        funcs.append(func_start)
-                # sects
-                break
+            segment_info = lc
             # lcs
             break
         # modules
         break
 
-    return funcs, module_file_spec
+    return module_file_spec, header_addr, slide, segment_info
