618855abf0dae80bb674c34c43d5de6dc455dd19
xyj <yyth-003@163.com>
2024-03-25 10:24:34 +0800

    添加类型声明

--- src/common.py
+++ src/common.py
@@ -2,21 +2,21 @@
 g_byteorder = 'little'
 
 
-def get_int(base, offset, byteorder=None):
+def get_int(base: bytes, offset, byteorder=None):
     if byteorder:
         return int.from_bytes(base[offset:offset + 4], byteorder=byteorder)
     else:
         return int.from_bytes(base[offset:offset + 4], byteorder=g_byteorder)
 
 
-def get_long(base, offset, byteorder=None):
+def get_long(base: bytes, offset, byteorder=None):
     if byteorder:
         return int.from_bytes(base[offset:offset + 8], byteorder=byteorder)
     else:
         return int.from_bytes(base[offset:offset + 8], byteorder=g_byteorder)
 
 
-def get_string(base, offset, length=0):
+def get_string(base: bytes, offset, length=0):
     if length == 0:
         pos = base.find(b'\x00', offset)
         length = pos - offset
@@ -30,7 +30,7 @@ def swap32(num):
             ((num >> 24) & 0x000000FF))
 
 
-def get_cs_super_blob(base, offset, byteorder=None):
+def get_cs_super_blob(base: bytes, offset, byteorder=None):
     magic = swap32(get_int(base, offset, byteorder))
     length = get_int(base, offset + 4, byteorder)
     count = swap32(get_int(base, offset + 8, byteorder))
@@ -38,14 +38,14 @@ def get_cs_super_blob(base, offset, byteorder=None):
     return magic, length, count
 
 
-def get_cs_blob_index(base, offset, byteorder=None):
+def get_cs_blob_index(base: bytes, offset, byteorder=None):
     data_type = get_int(base, offset, byteorder)
     data_offset = swap32(get_int(base, offset + 4, byteorder))
 
     return data_type, data_offset
 
 
-def get_cs_blob(base, offset, byteorder=None):
+def get_cs_blob(base: bytes, offset, byteorder=None):
     magic = swap32(get_int(base, offset, byteorder))
     length = swap32(get_int(base, offset + 4, byteorder))
 
